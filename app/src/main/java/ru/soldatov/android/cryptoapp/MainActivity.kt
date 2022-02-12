1566794dceda3d218c7595a49651f8a71ce34766
Егор Солдатов <soldatovea93@gmail.com>
2022-02-12 18:34:17 +0300

    Add network connection

--- app/src/main/java/ru/soldatov/android/cryptoapp/MainActivity.kt
+++ app/src/main/java/ru/soldatov/android/cryptoapp/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.soldatov.android.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import ru.soldatov.android.cryptoapp.api.ApiFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        compositeDisposable.add(disposable())
+    }
+
+    private fun disposable(): Disposable {
+        return ApiFactory.apiServer.getFullPriceList(fSyms = "BTC")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_MAIN_ACTIVITY", it.toString())
+            }, {
+                Log.d("TEST_MAIN_ACTIVITY", it.message!!)
+            })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/ru/soldatov/android/cryptoapp/api/ApiService.kt
@@ -0,0 +1,40 @@
+package ru.soldatov.android.cryptoapp.api
+
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.soldatov.android.cryptoapp.pojo.CoinInfoListOfData
+import ru.soldatov.android.cryptoapp.pojo.CoinPriceInfoRawData
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_API_KEY) apyKey: String = API_KEY,
+        @Query(QUERY_LIMIT) limit: Int = LIMIT,
+        @Query(QUERY_TO_SYMBOL) tSym: String = CURRENCY_SYMBOL
+    ) : Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_API_KEY) apyKey: String = API_KEY,
+        @Query(QUERY_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_TO_SYMBOLS) tSyms: String = CURRENCY_SYMBOL
+    ): Single<CoinPriceInfoRawData>
+
+    companion object {
+
+        private const val QUERY_API_KEY = "api_key"
+        private const val QUERY_LIMIT = "limit"
+        private const val QUERY_TO_SYMBOL = "tsym"
+        private const val QUERY_FROM_SYMBOLS = "fsyms"
+        private const val QUERY_TO_SYMBOLS = "tsyms"
+
+        private const val CURRENCY_SYMBOL = "USD"
+
+        private const val LIMIT = 10
+        private const val API_KEY =
+            "92a635085ff9e9680d9814b857bf0e3269bf3e773b92735084d9d91cef56e4b8"
+    }
+
+}
